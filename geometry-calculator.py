ad3f9cfdc30d455b464939189fed0716a4c9f21d
himani0550 <himani0550@gmail.com>
2023-07-31 18:14:40 +0530

    adding circle changes

--- geometry-calculator.py
+++ geometry-calculator.py
@@ -46,9 +46,9 @@ print(f"The area of the circle with radius {radius} ={calculator.calculate_circl
  
 
 # TODO: Implement the feature to calculate the area of a rectangle # 
-length = 10
+#length = 10
 
-width = 6
+#width = 6
 
-print(f"The area of the rectangle with length {length} and width {width} ={calculator.calculate_rectangle_area(length, width)}")
+#print(f"The area of the rectangle with length {length} and width {width} ={calculator.calculate_rectangle_area(length, width)}")
 
